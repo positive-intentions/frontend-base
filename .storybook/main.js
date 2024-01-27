d1c15597523a4bd129e94f6d19965fe25a10d604
xoron <rmaher84@gmail.com>
2024-01-27 10:23:12 +0000

    test

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ const config = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     'storybook-dark-mode',
   ],
