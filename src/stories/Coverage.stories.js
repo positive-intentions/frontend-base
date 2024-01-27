6d35fd0e6374ee571bd1f44bda4f8a81e8217d03
xoron <rmaher84@gmail.com>
2024-01-27 15:07:45 +0000

    adding tests and coverage report

---
+++ src/stories/Coverage.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Example from './components/Example.tsx';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Coverage/Coverage',
+  component: () => <iframe
+    src="coverage/"
+    style={{height: '99vh', width: '100%', border: 0 }} />,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+    // controls off
+    options: { showPanel: false },
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+//   tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+//   argTypes: {
+//     backgroundColor: { control: 'color' },
+//   },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Coverage = {};
+
+// export const Secondary = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+
+// export const Large = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+
+// export const Small = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
